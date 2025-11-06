c2eefbb8388f09574158a0311e2a8095feeb1a68
ExMozarella <155804607+ExMozarella@users.noreply.github.com>
2025-11-06 10:35:41 +0700

    Update koneksi.php

--- belajar_regist/koneksi.php
+++ belajar_regist/koneksi.php
@@ -1,15 +1,9 @@
 <?php
-// Konfigurasi koneksi database
-$host = "localhost"; // Nama server database
-$user = "root";      // Username database (default AMPPS adalah "root")
-$pass = "mysql";     // <<<<<< UBAH BAGIAN INI
-$db   = "data";      // Nama database yang sudah kita buat
 
-// Membuat koneksi
-$koneksi = mysqli_connect($host, $user, $pass, $db);
-
-// Cek koneksi
-if (!$koneksi) {
+$koneksi = mysqli_connect("localhost","root","mysql","data");
+// echo "koneksi berhasil";
+if(!$koneksi){
     die("Koneksi gagal: " . mysqli_connect_error());
 }
+
 ?>
